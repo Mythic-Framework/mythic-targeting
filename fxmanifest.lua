b98c5a6f2af79c4577c2d5a8f73490ebb750400d
Artmines <arturowells@gmail.com>
2026-04-11 03:37:35 -0230

    bump

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
-version '1.0.0'
+version '1.0.1'
 repository 'https://github.com/Mythic-Framework/mythic-targeting'
 client_script "@mythic-base/components/cl_error.lua"
 client_script "@mythic-pwnzor/client/check.lua"
